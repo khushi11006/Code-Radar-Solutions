9cf6d07388c679033a3efb2962dd9df8cae8d2f6
Khushleen Kaur <khushildh2000@gmail.com>
2025-04-04 20:29:55 +0530

    Solution to Check if Array is Sorted

--- Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
+++ Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
@@ -1,24 +1,17 @@
-#include <stdio.h>
 #include <stdbool.h>
 
 bool isNonDescending(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         if (arr[i] > arr[i + 1]) {
-            return false;  // Found a descending pair
+            return false;
         }
     }
-    return true;  // All pairs are non-descending
+    return true;
 }
 
 int main() {
     int arr[] = {1, 2, 2, 3, 4};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    if (isNonDescending(arr, n)) {
-        printf("%d\n");
-    } else {
-        printf("%d\n");
-    }
-
-    return 0;
+    return isNonDescending(arr, n);  // returns 1 if sorted, 0 if not
 }
